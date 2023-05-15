4e8db34732f3b75a0dab2459da15edbc250c7dad
javie <javie@LAPTOP-68FO9DBP>
2023-05-15 13:11:01 +0200

    Hago la suma de a+b que me pidio Javi.

--- IFCT0609_2023_Github1/src/github/HelloWorld.java
+++ IFCT0609_2023_Github1/src/github/HelloWorld.java
@@ -3,8 +3,10 @@ package github;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		int a=5;
+		int b=25;
 		System.out.println("Hola mundo!!");
-
+		System.out.println("Esta es la suma de " + a + " + "+ b + ":" + (a+b));
 	}
 
 }
